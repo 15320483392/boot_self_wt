f475a386b2039301cc85fe8eec1eda909f93ba6a
Wtar <WTarZoom@163.com>
2018-09-11 17:28:35 +0800

    集成Generator

--- src/main/java/com/example/boot_self_wt/BootSelfWtApplication.java
+++ src/main/java/com/example/boot_self_wt/BootSelfWtApplication.java
@@ -1,10 +1,14 @@
 package com.example.boot_self_wt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@MapperScan("com.example.boot_self_wt.domain")
 @SpringBootApplication
+@ComponentScan({"com.example.boot_self_wt"})
 @EnableAutoConfiguration(exclude = {
 		/*关闭Security拦截器*/
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
